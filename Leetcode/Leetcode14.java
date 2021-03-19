8e29e8b113287288f5101697e5324b66b9204636
cocokaribou <younginlee2017@gmail.com>
2021-03-19 16:20:04 +0900

    Modified Leetcode14.java

--- Leetcode/Leetcode14.java
+++ Leetcode/Leetcode14.java
@@ -24,52 +24,30 @@
 import java.util.*;
 
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
+    public String longestCommonPrefix(String[] strs){
+        if(strs.length == 0) return "";
+        if(strs.length == 1) return strs[0];
+        StringBuilder result = new StringBuilder();
         
-        //글자수순으로 오름차순 정렬부터
-        Arrays.sort(strs, Comparator.comparingInt(String::length));
         
-        //string 배열의 요소가 하나일 경우(단어가 하나 주어질 경우), 그대로 return
-        if(strs.length==1){
-            return strs[0];
-        }
-                
-        //string 배열의 요소를 char로 쪼개서 2차원 char배열에 저장
-        char[][] charArr = new char[strs.length][];
-        for(int i=0; i<strs.length; i++){
-            charArr[i]= strs[i].toCharArray();
-        }
-
-        String prefix = "";
-
-        //여기가 이제 골때리는데...
-
-        boolean checker = false;
-        for(int i=0; i<charArr.length; i++){
-            for(int j=0; j<charArr[i].length; j++){
-                
-                while(i!=charArr.length-1){
-                    if(charArr[i][j] == charArr[i+1][j]){
-                        checker = true;
-                        i++;
-                    }else{
-                        checker = false;
-                        prefix = (prefix.length()>1)? prefix = prefix.substring(0, prefix.length()-1) : "";
-                        i++;
-                    }
-                }if(checker){
-                    prefix = prefix+""+charArr[i][j];
-                }
+        for(int i =0; i<strs[0].length(); i++){
+            char compared = strs[0].charAt(i);
+            
+            for(int j = 1; j<strs.length; j++){
+                if(compared != strs[j].charAt(i)) return result.toString();
             }
+            result.append(compared);
+            
         }
-        return prefix;
+        
+        return result.toString();
     }
 }
 
 public class Main{
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String[] strs = {"flat", "flow", "flower"};
+        String[] strs = {"dog","dat"};
         String result = sol.longestCommonPrefix(strs);
         System.out.println(result);
     }
