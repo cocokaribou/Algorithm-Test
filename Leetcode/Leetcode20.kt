5bc500c6dfcc84f9a5fc31a5982e15a59992ada8
cocokaribou <younginlee2017@gmail.com>
2021-06-20 16:07:17 +0900

    [Update] minor changes in logic

--- Leetcode/Leetcode20.kt
+++ Leetcode/Leetcode20.kt
@@ -7,22 +7,23 @@
 // "[(){}]" -> true
 // "({)}" -> false
 
+package com.example.algorithm_solutions.classes
 
 class Leetcode20 {
 
-	/**
-	* isValid()
-	* @param s Input String
-	* @return Returns if the input has a valid parentheses format 
-	**/
+    /**
+     * isValid()
+     * @param s Input String
+     * @return Returns if the input has a valid parentheses format
+     **/
 
     fun isValid(s: String): Boolean {
 
-        if(s.length % 2 == 1){
+        if (s.length % 2 == 1) {
             return false
         }
 
-        if(!s.startsWith("(") || !s.startsWith("{") || !s.startsWith("[")){
+        if (!(s.startsWith("(") || s.startsWith("{") || s.startsWith("["))) {
             return false
         }
 
@@ -30,30 +31,31 @@ class Leetcode20 {
         var i = 0
         var flag = false
 
-        while(i != charSeq.length){
-
-            var charDiff:Int = when(charSeq[i]){
+        while (i != charSeq.length-1) {
+            if(i == charSeq.length){
+                return true
+            }
+            var charDiff: Int = when (charSeq[i]) {
                 '(' -> 1
                 '{', '[' -> 2
-                else -> 0
+                else -> return flag
             }
 
             var comparator = charSeq[i]
-            var next = charSeq[i+1]
+            var next = charSeq[i + 1]
 
-            var indexDiff = charSeq.length-(i*2+1)
-            var last = charSeq[i+indexDiff]
+            var indexDiff = charSeq.length - (i * 2 + 1)
+            var last = charSeq[i + indexDiff]
 
-            if(next - comparator == charDiff){
+            if (last - comparator == charDiff) {
+                flag = true
+                i ++
+            } else if (next - comparator == charDiff) {
                 flag = true
                 i += 2
-            }
-            else if(last - comparator == charDiff) {
-                flag = true
-                i++
-            }
-            else {
+            } else {
                 flag = false
+                i++
             }
         }
         return flag
