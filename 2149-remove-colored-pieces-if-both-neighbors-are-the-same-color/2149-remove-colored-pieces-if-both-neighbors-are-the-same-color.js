f00d8bb18f2cef4cc3f2e68333fc8a11680c8be2
Youngin Lee <49682500+cocokaribou@users.noreply.github.com>
2025-03-30 11:47:53 +0900

    Time: 15 ms (59.09%), Space: 57.6 MB (40.91%) - LeetHub

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} colors
+ * @return {boolean}
+ */
+var winnerOfGame = function(colors) {
+    let aliceMoves = 0;
+    let bobMoves = 0;
+
+    for (let i = 1; i < colors.length -1; i++) {
+        if (colors[i-1] === colors[i] && colors[i] === colors[i+1]) {
+            colors[i] === 'A' ? aliceMoves++ : bobMoves++;
+        }
+    }
+    return aliceMoves > bobMoves;
+};
